14cc4da07f651d5f891a20b26e1e949bb677bab9
Milan Brčkalo <mbrckalo997@outlook.com>
2021-03-29 00:02:36 +0200

    Nacrt CRUD operacija

--- Starter-project/src/main/java/rva/ctrls/ObrazovanjeRestController.java
+++ Starter-project/src/main/java/rva/ctrls/ObrazovanjeRestController.java
@@ -3,8 +3,15 @@ package rva.ctrls;
 import java.util.Collection;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import rva.jpa.Obrazovanje;
@@ -12,6 +19,9 @@ import rva.repository.ObrazovanjeRepository;
 
 @RestController
 public class ObrazovanjeRestController {
+	@Autowired
+	private JdbcTemplate jdbcTemplate;
+	
 	@Autowired
 	private ObrazovanjeRepository obrazovanjeRepository;
 	
@@ -23,5 +33,44 @@ public class ObrazovanjeRestController {
 	@GetMapping("obrazovanje/{id}")
 	public Obrazovanje get(@PathVariable("id") Integer id) {
 		return obrazovanjeRepository.getOne(id);
+	}
+	
+	@PostMapping("obrazovanje")
+	public ResponseEntity<Obrazovanje> insertObrazovanje(@RequestBody Obrazovanje obrazovanje) {
+		if (!obrazovanjeRepository.existsById(obrazovanje.getId())) {
+			obrazovanjeRepository.save(obrazovanje);
+			
+			return new ResponseEntity<Obrazovanje>(HttpStatus.OK);
+		}
+		
+		return new ResponseEntity<Obrazovanje>(HttpStatus.CONFLICT);
+	}
+	
+	@PutMapping("obrazovanje")
+	public ResponseEntity<Obrazovanje> updateObrazovanje(@RequestBody Obrazovanje obrazovanje) {
+		if (!obrazovanjeRepository.existsById(obrazovanje.getId())) {
+			return new ResponseEntity<Obrazovanje>(HttpStatus.NO_CONTENT);
+		}
+		
+		obrazovanjeRepository.save(obrazovanje);
+		
+		return new ResponseEntity<Obrazovanje>(HttpStatus.OK);
+	}
+	
+	@DeleteMapping("obrazovanje/{id}")
+	public ResponseEntity<Obrazovanje> deleteObrazovanje(@PathVariable("id") Integer id) {
+		if (!obrazovanjeRepository.existsById(id)) {
+			return new ResponseEntity<Obrazovanje>(HttpStatus.NO_CONTENT);
+		}
+		
+		obrazovanjeRepository.deleteById(id);
+		
+		if (id == -100) {
+			String sql = "INSERT INTO \"obrazovanje\"(\"id\", \"naziv\", \"stepen_strucne_spreme\", \"opis\") VALUES (-100, 'ObrazovanjeNaziv', 'ObrazovanjeSSS', 'ObrazovanjeOpis')";
+			
+			jdbcTemplate.execute(sql);
+		}
+		
+		return new ResponseEntity<Obrazovanje>(HttpStatus.OK);
 	}
 }
--- Starter-project/src/main/java/rva/ctrls/PreduzeceRestController.java
+++ Starter-project/src/main/java/rva/ctrls/PreduzeceRestController.java
@@ -3,8 +3,15 @@ package rva.ctrls;
 import java.util.Collection;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import rva.jpa.Preduzece;
@@ -12,6 +19,9 @@ import rva.repository.PreduzeceRepository;
 
 @RestController
 public class PreduzeceRestController {
+	@Autowired
+	private JdbcTemplate jdbcTemplate;
+	
 	@Autowired
 	private PreduzeceRepository preduzeceRepository;
 	
@@ -23,5 +33,44 @@ public class PreduzeceRestController {
 	@GetMapping("preduzece/{id}")
 	public Preduzece get(@PathVariable("id") Integer id) {
 		return preduzeceRepository.getOne(id);
+	}
+	
+	@PostMapping("preduzece")
+	public ResponseEntity<Preduzece> insertPreduzece(@RequestBody Preduzece preduzece) {
+		if (!preduzeceRepository.existsById(preduzece.getId())) {
+			preduzeceRepository.save(preduzece);
+			
+			return new ResponseEntity<Preduzece>(HttpStatus.OK);
+		}
+		
+		return new ResponseEntity<Preduzece>(HttpStatus.CONFLICT);
+	}
+	
+	@PutMapping("preduzece")
+	public ResponseEntity<Preduzece> updatePreduzece(@RequestBody Preduzece preduzece) {
+		if (!preduzeceRepository.existsById(preduzece.getId())) {
+			return new ResponseEntity<Preduzece>(HttpStatus.NO_CONTENT);
+		}
+		
+		preduzeceRepository.save(preduzece);
+		
+		return new ResponseEntity<Preduzece>(HttpStatus.OK);
+	}
+	
+	@DeleteMapping("preduzece/{id}")
+	public ResponseEntity<Preduzece> deletePreduzece(@PathVariable("id") Integer id) {
+		if (!preduzeceRepository.existsById(id)) {
+			return new ResponseEntity<Preduzece>(HttpStatus.NO_CONTENT);
+		}
+		
+		preduzeceRepository.deleteById(id);
+		
+		if (id == -100) {
+			String sql = "INSERT INTO \"preduzece\"(\"id\", \"naziv\", \"opis\", \"pib\", \"sediste\") VALUES (-100, 'PreduzeceNaziv', 'PreduzeceOpis', 'PreduzecePIB', 'PreduzeceSediste')";
+			
+			jdbcTemplate.execute(sql);
+		}
+		
+		return new ResponseEntity<Preduzece>(HttpStatus.OK);
 	}
 }
