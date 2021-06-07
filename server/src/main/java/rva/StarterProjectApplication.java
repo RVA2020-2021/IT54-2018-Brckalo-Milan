880eb23c2678a289a57e7c0bb4337930e02846d3
Milan Brčkalo <mbrckalo997@outlook.com>
2021-06-07 20:50:53 +0200

    API deployment

--- server/src/main/java/rva/StarterProjectApplication.java
+++ server/src/main/java/rva/StarterProjectApplication.java
@@ -2,9 +2,18 @@ package rva;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
+@Controller
 @SpringBootApplication
 public class StarterProjectApplication {
+	@RequestMapping("/")
+    @ResponseBody
+    String home() {
+      return "Hello from PetiZadatak API!";
+    }
 
 	public static void main(String[] args) {
 		SpringApplication.run(StarterProjectApplication.class, args); 
